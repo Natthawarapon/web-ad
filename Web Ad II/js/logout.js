7e692918717e73e2db453b86a82d7625f023d014
ipeeranan <stroms31@gmail.com>
2019-11-25 22:48:25 +0700

    add customer

--- Web Ad II/js/logout.js
+++
@@ -1,8 +0,0 @@
-function logout() {
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-        window.location.href = 'login.html';
-    }).catch(function (error) {
-        // An error happened.
-    });
-}
